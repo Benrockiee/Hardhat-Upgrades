989b48768ae0a0773ab32c02f7c23b33c57c11ec
Benjamin izuchukwu umeike <benrockiee@gmail.com>
2022-10-07 02:21:41 -0700

    Added comments

--- contracts/sublesson/SmallProxy.sol
+++ contracts/sublesson/SmallProxy.sol
@@ -4,17 +4,31 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/proxy/Proxy.sol";
 
+//To deal with proxies, we really dont wanna have anything in storage because if we do
+//delegate call and that delegate call changes some storage, we are gonna screw up our
+//contracts storage but we still need to store that implementations address somewhere so
+//we can call it so EIP-1976 is called the standard proxy storage slots which is an EIP
+//for having certain storage slots specifically used for proxies.
+
+//so down below we set bytes32 implementation slot to that location in storage and whatever
+//is in that storage slot is going to be our implementation address so the way this is going to work
+//is that any contract that calls this proxy contract, if its not this set implementation function,
+//its going to pass it over to what is inside the implementation slot address.
+
 contract SmallProxy is Proxy {
     // This is the keccak-256 hash of "eip1967.proxy.implementation" subtracted by 1
     bytes32 private constant _IMPLEMENTATION_SLOT =
         0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
+    //This will change where those delegate calls are gonna be sending
+    //This can be equivalent with upgrading your smart contracts
     function setImplementation(address newImplementation) public {
         assembly {
             sstore(_IMPLEMENTATION_SLOT, newImplementation)
         }
     }
 
+    //Then we have implementation here to read where that implementation contract is
     function _implementation() internal view override returns (address implementationAddress) {
         assembly {
             implementationAddress := sload(_IMPLEMENTATION_SLOT)
